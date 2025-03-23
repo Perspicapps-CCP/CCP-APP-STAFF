7d653b0ace2d576a707cfa41cb62fd8675b0d46a
Andres Clavijo Rodriguez <a.clavijo1@uniandes.edu.co>
2025-03-23 17:55:18 -0500

    feat: enhance login component with snackbar notifications for error messages

--- src/app/modules/auth/paginas/login/login.component.ts
+++ src/app/modules/auth/paginas/login/login.component.ts
@@ -3,12 +3,15 @@ import { MatCardModule } from '@angular/material/card';
 import { sharedImports } from '../../../../shared/otros/shared-imports';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { LoginService } from '../../servicios/login.service';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-login',
   imports: [
     sharedImports,
     MatCardModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MatSnackBarModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -21,17 +24,21 @@ export class LoginComponent {
 
   constructor(
     private loginService: LoginService,
+    private _snackBar: MatSnackBar,
+    private translate: TranslateService
   ) { }
 
   iniciarSesion() {
     let loginForm = this.loginForm.value;
     if (loginForm.username && loginForm.password) {
       this.loginService.iniciarSesion(loginForm.username, loginForm.password).subscribe({
-        next: (res) => {
-          console.log('Inicio de sesión exitoso', res);
-        },
-        error: (err) => {
-          console.error('Error al iniciar sesión', err);
+        error: () => {
+          this.translate.get('LOGIN.ERROR_MESSAGE').subscribe((mensaje: string) => {
+            this._snackBar.open(mensaje, '', {
+              horizontalPosition: 'end',
+              verticalPosition: 'top',
+            });
+          });
         }
       });
       console.log('Iniciar sesión');
