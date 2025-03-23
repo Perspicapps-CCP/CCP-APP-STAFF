18f557b61c14d9e1029b0996a20301ae4a59d98e
Andres Clavijo Rodriguez <a.clavijo1@uniandes.edu.co>
2025-03-23 14:01:16 -0500

    feat: implement login component with localization, styling, and form functionality

--- src/app/modules/auth/paginas/login/login.component.ts
+++ src/app/modules/auth/paginas/login/login.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { sharedImports } from '../../../../shared/otros/shared-imports';
 @Component({
   selector: 'app-login',
   imports: [
+    sharedImports,
+    MatCardModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+
+  iniciarSesion() {
+    console.log('Iniciar sesión');
+  }
 }
