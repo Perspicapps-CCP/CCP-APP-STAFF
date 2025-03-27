5a708d4cf77621f7426de9ec8687df32a3b80799
Andres Clavijo Rodriguez <a.clavijo1@uniandes.edu.co>
2025-03-27 13:23:45 -0500

    feat: enhance unit tests for ProductosFabricanteComponent; improve localization service mock and register locale data

--- src/app/modules/fabricantes/componentes/productos-fabricante/productos-fabricante.component.spec.ts
+++ src/app/modules/fabricantes/componentes/productos-fabricante/productos-fabricante.component.spec.ts
@@ -4,7 +4,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { TranslateModule, TranslateLoader, TranslateFakeLoader, TranslateService } from '@ngx-translate/core';
 import { of } from 'rxjs';
 import { BehaviorSubject } from 'rxjs';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { LOCALE_ID, NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { ProductosFabricanteComponent } from './productos-fabricante.component';
 import { FabricantesService } from '../../servicios/fabricantes.service';
@@ -13,40 +13,20 @@ import { HighlightTextPipe } from '../../../../shared/pipes/highlight-text.pipe'
 import { LocalCurrencyPipe } from '../../../../shared/pipes/local-currency.pipe';
 import { VisorImagenesDialogComponent } from '../../../../shared/componentes/visor-imagenes-dialog/visor-imagenes-dialog.component';
 import { LocalizationService } from '../../../../shared/servicios/localization.service';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
 
 // Mock mejorado para LocalizationService
-export class MockLocalizationService {
-  currentLocalizationSubject = new BehaviorSubject<any>({
-    decimal: ',',
-    thousands: '.',
-    grouping: [3],
-    currency: ['€', ''],
-    dateTime: '%a %d %b %Y %X',
-    date: '%d/%m/%Y',
-    time: '%H:%M:%S',
-    periods: ['AM', 'PM'],
-    days: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-    shortDays: ['Dom', 'Lun', 'Mar', 'Mié', 'Jue', 'Vie', 'Sáb'],
-    months: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-    shortMonths: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic']
-  });
+class MockLocalizationService {
+  currentLocalizationSubject = new BehaviorSubject<any>({});
   currentLocalization$ = this.currentLocalizationSubject.asObservable();
   currentLang$ = new BehaviorSubject<string>('es').asObservable();
   localeId = 'es-ES';
-  currentLocale$ = new BehaviorSubject<string>('es-ES').asObservable();
-
-  formatNumberFactory = jasmine.createSpy('formatNumberFactory').and.returnValue(
-    (value: number) => value.toLocaleString('es-ES')
-  );
-
-  initializeLanguage = jasmine.createSpy('initializeLanguage');
+  currentLocale$ = new BehaviorSubject<string>('es-ES');
 
   getLocale() { return 'es-ES'; }
   getLang() { return 'es'; }
   getCurrencyCode() { return 'EUR'; }
-  formatCurrency(value: any) { return `€${value}`; }
-  formatNumber(value: any) { return value.toLocaleString('es-ES'); }
-  formatDate(value: any) { return new Date(value).toLocaleDateString('es-ES'); }
 }
 
 describe('ProductosFabricanteComponent', () => {
@@ -71,6 +51,8 @@ describe('ProductosFabricanteComponent', () => {
     open: jasmine.createSpy('open')
   };
 
+  registerLocaleData(localeEs);
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -87,6 +69,7 @@ describe('ProductosFabricanteComponent', () => {
         { provide: DinamicSearchService, useValue: dinamicSearchServiceMock },
         { provide: MatDialog, useValue: dialogMock },
         { provide: LocalizationService, useClass: MockLocalizationService },
+        { provide: LOCALE_ID, useValue: 'es-ES' },
         TranslateService
       ],
       schemas: [NO_ERRORS_SCHEMA]
