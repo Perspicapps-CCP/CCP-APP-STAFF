5dbd95e1d907462d87f00893aad2f8117adf2e4d
andresalarcon <w.alarconc@uniandes.edu.co>
2025-04-11 22:28:40 -0500

    Se crea autocomplete para seleccionar paises y ciudades

--- src/app/modules/bodegas/componentes/agregar-bodega/agregar-bodega.component.ts
+++ src/app/modules/bodegas/componentes/agregar-bodega/agregar-bodega.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -7,14 +7,18 @@ import { BodegasService } from '../../servicios/bodegas.service';
 import { CommonModule } from '@angular/common';
 import { OnlyNumbersDirective } from '../../../../shared/directivas/only-numbers.directive';
 import { Bodega } from '../../interfaces/bodega.interface';
+import { debounceTime, distinctUntilChanged, map, Observable, OperatorFunction } from 'rxjs';
+import { Pais } from '../../interfaces/paises.interface';
+import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
+import { Ciudad } from '../../interfaces/ciudades.interface';
 
 @Component({
   selector: 'app-agregar-bodega',
-  imports: [ReactiveFormsModule, TranslateModule, CommonModule, OnlyNumbersDirective],
+  imports: [ReactiveFormsModule, TranslateModule, CommonModule, OnlyNumbersDirective, NgbTypeaheadModule],
   templateUrl: './agregar-bodega.component.html',
   styleUrl: './agregar-bodega.component.scss',
 })
-export class AgregarBodegaComponent {
+export class AgregarBodegaComponent implements OnInit {
   readonly dialogRef = inject(MatDialogRef<AgregarBodegaComponent>);
 
   bodegaForm = new FormGroup({
@@ -38,12 +42,25 @@ export class AgregarBodegaComponent {
     }),
   });
 
+  countries: { name: string; iso2: string }[] = [];
+  cities: Ciudad[] = [];
   constructor(
     private wareHouseService: BodegasService,
     private _snackBar: MatSnackBar,
     private translate: TranslateService,
   ) {}
 
+  ngOnInit(): void {
+    this.obtenerPaises();
+  }
+
+  obtenerPaises() {
+    this.wareHouseService.obtenerPaises().subscribe(res => {
+      this.countries = res;
+    });
+  }
+
+  formatter = (x: { name: string }) => x.name;
   getErrorMessage(controlName: string): { key: string; params?: any } {
     if (this.bodegaForm.get(controlName)?.hasError('required')) {
       return {
@@ -118,5 +135,46 @@ export class AgregarBodegaComponent {
         this.dialogRef.close();
       },
     });
+  }
+
+  searchCountries: OperatorFunction<string, readonly { name: string }[]> = (
+    text$: Observable<string>,
+  ) =>
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term =>
+        term.length < 2
+          ? []
+          : this.countries.filter(country => new RegExp(term, 'mi').test(country.name)).slice(0, 3),
+      ),
+    );
+
+  onCountrySelected(event: any) {
+    const selectedCountry = event.item;
+    this.cities = [];
+    this.bodegaForm.get('city')?.reset();
+    this.wareHouseService.obtenerCuidadesPais(selectedCountry.iso2).subscribe(res => {
+      this.cities = res;
+    });
+  }
+
+  isValidCountry() {
+    const inputValue = this.bodegaForm.get('country')?.value?.trim();
+
+    if (typeof inputValue === 'string') {
+      const foundCountry = this.countries.find(
+        c => c.name.toLowerCase() === inputValue.toLowerCase(),
+      );
+      if (!foundCountry) {
+        this.bodegaForm.get('country')?.reset();
+        this.bodegaForm.get('city')?.reset();
+        this.cities = [];
+      } else {
+        this.bodegaForm.get('country')?.setValue(foundCountry.name);
+      }
+      this.bodegaForm.get('country')?.markAsTouched();
+      this.bodegaForm.get('country')?.updateValueAndValidity();
+    }
   }
 }
--- src/app/modules/productos/servicios/productos.service.spec.ts
+++ src/app/modules/productos/servicios/productos.service.spec.ts
@@ -5,6 +5,8 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { environment } from '../../../../environments/environment';
 import { ProductosService } from './productos.service';
+import { Vendedor } from '../../vendedores/interfaces/vendedores.interface';
+import { Producto } from '../interfaces/productos.interface';
 
 describe('ProductosService', () => {
   let service: ProductosService;
@@ -27,5 +29,45 @@ describe('ProductosService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('debería obtener la lista de vendedores', () => {
+    // Mock de datos de respuesta
+    const mockProductos: Producto[] = [
+      {
+        product_id: '1',
+        product_name: 'Product1',
+        product_code: 'p001',
+        manufacturer_name: 'Manufacturer',
+        price: 12345678,
+        images: ['image'],
+        quantity: '1',
+        warehouse_name: 'WareHouse',
+      },
+      {
+        product_id: '1',
+        product_name: 'Product1',
+        product_code: 'p001',
+        manufacturer_name: 'Manufacturer',
+        price: 12345678,
+        images: ['image'],
+        quantity: '1',
+        warehouse_name: 'WareHouse',
+      },
+    ];
+
+    // Hacemos la llamada al método
+    let result: Producto[] | undefined;
+    service.obtenerProductos().subscribe(productos => {
+      result = productos;
+    });
+
+    // Simulamos la respuesta HTTP
+    const req = httpMock.expectOne(`${apiUrl}/inventory/stock/products`);
+    expect(req.request.method).toBe('GET');
+    req.flush(mockProductos);
+
+    // Verificamos el resultado
+    expect(result).toEqual(mockProductos);
   });
 });
