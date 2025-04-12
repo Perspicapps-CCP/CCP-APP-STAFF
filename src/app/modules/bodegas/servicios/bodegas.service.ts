c134425a55a445ea23cf0354f707d0cb32b70a7d
walarconc <w.alarconc@uniandes.edu.co>
2025-04-12 09:29:20 -0500

    Se crea autocomplete para seleccionar paises y ciudades (#17)
    * Se crea autocomplete para seleccionar paises y ciudades
    
    * Se identa codigo

--- src/app/modules/bodegas/servicios/bodegas.service.ts
+++ src/app/modules/bodegas/servicios/bodegas.service.ts
@@ -1,18 +1,22 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, switchMap } from 'rxjs';
+import { map, Observable, switchMap, tap } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { ProductoFabricante } from '../../fabricantes/interfaces/producto-fabricante.interface';
 import { Bodega } from '../interfaces/bodega.interface';
 import { ProductoBodega, ProductoBodegaInventario } from '../interfaces/producto-bodega';
 import { MasivoProductosResponse } from '../interfaces/masivo-productos-bodega-response';
 import { Fabricante } from '../../fabricantes/interfaces/fabricantes.interface';
+import { Producto } from '../../productos/interfaces/productos.interface';
+import { Pais } from '../interfaces/paises.interface';
+import { Ciudad } from '../interfaces/ciudades.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class BodegasService {
   private apiUrl = environment.apiUrlCCP;
+  private apiKey = 'U1o2V1lSMDh0b1B1YTVEM3hWTkdBOVB1UnI5WmhrRmw2YTVpQ2Nhcw==';
 
   constructor(private http: HttpClient) {}
 
@@ -81,5 +85,21 @@ export class BodegasService {
 
   crearBodega(bodega: Bodega) {
     return this.http.post<Bodega>(`${this.apiUrl}/inventory/warehouse`, bodega);
+  }
+
+  obtenerPaises() {
+    const headers = new HttpHeaders({
+      'X-CSCAPI-KEY': this.apiKey,
+    });
+    return this.http.get<Pais[]>('https://api.countrystatecity.in/v1/countries', { headers });
+  }
+
+  obtenerCuidadesPais(iso2: string) {
+    const headers = new HttpHeaders({
+      'X-CSCAPI-KEY': this.apiKey,
+    });
+    return this.http.get<Ciudad[]>(`https://api.countrystatecity.in/v1/countries/${iso2}/cities`, {
+      headers,
+    });
   }
 }
